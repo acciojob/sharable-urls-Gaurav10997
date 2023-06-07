65b1e768695b7e48e259cae99842fb1bfab82191
Gaurav Kumar Mandal <96892818+Gaurav10997@users.noreply.github.com>
2023-06-07 17:53:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 // your code here
+const tag =document.getElementById("tag");
+const btn = document.getElementById("button");
+var nameInput = document.querySelector('input[name="name"]');
+var yearInput = document.querySelector('input[name="year"]');
+// function newValue()
+
+btn.addEventListener("click" , function(e){
+	e.preventDefault();
+	if(nameInput.value && yearInput.value){
+		tag.textContent += `/?name=${nameInput.value}&year=${yearInput.value}`
+	}else if(nameInput.value ){
+		tag.textContent += `/?name=${nameInput.value}`
+	}else if(yearInput.value ){
+		tag.textContent += `/?name=${yearInput.value}`
+	}
+	nameInput.value="";
+	yearInput.value="";
+	
+
+})
